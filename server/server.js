1659902f8a70d50f0438e3583050a5b6322c08ad
dinesh061 <115059054+dinesh061@users.noreply.github.com>
2023-07-08 08:22:11 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth");
 const app = express();
+const path = require("path");
 
 app.use(
   cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })

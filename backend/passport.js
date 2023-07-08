e31bf174235df52398349b8a6134e8acb59f5ca1
dinesh061 <115059054+dinesh061@users.noreply.github.com>
2023-07-08 16:50:23 +0530

    Update passport.js

--- backend/passport.js
+++ backend/passport.js
@@ -4,11 +4,11 @@ const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
 const GOOGLE_CLIENT_ID =
-  "your id";
-const GOOGLE_CLIENT_SECRET = "your id";
+  "881012707504-9qqefl2jh7u936dl6itknmpibhevntpi.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 
-GITHUB_CLIENT_ID = "your id";
-GITHUB_CLIENT_SECRET = "your id";
+GITHUB_CLIENT_ID = "f6b6af654aaaf640626f";
+GITHUB_CLIENT_SECRET = "ae8374e35c104611577db56f1e47d0f1b35b4c85";
 
 FACEBOOK_APP_ID = "your id";
 FACEBOOK_APP_SECRET = "your id";
